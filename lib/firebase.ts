fd5900e585e6cb1bc452867463118a97d03b35b0
Hebert Sandinha <hebertsandinha@gmail.com>
2025-11-30 17:08:24 -0300

    .
    .

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,11 +8,15 @@ const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
 // Initialize Firebase (avoid re-init on client hot reload)
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
+const analytics = typeof window !== 'undefined' && firebaseConfig.measurementId ? getAnalytics(app) : null;
 const auth = getAuth(app);
 auth.languageCode = 'pt';
 
