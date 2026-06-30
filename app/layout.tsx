eec336ae38c8f2c7829489568e5defd29c9baba1
Hebert Sandinha <hebertsandinha@gmail.com>
2026-06-30 15:09:04 -0300

    .
    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { SessionProvider } from "@/components/providers/SessionProvider";
 import { ThemeProvider } from "@/lib/contexts/ThemeContext";
 import "./globals.css";
@@ -33,7 +34,32 @@ export default function LoginLayout({
             * { box-sizing: border-box; }
           `
         }} />
+        {/* Meta Pixel Code */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1943444466371989&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
+      {/* Meta Pixel Script */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1943444466371989');
+          fbq('track', 'PageView');
+        `}
+      </Script>
       <body suppressHydrationWarning={true}>
         <SessionProvider>
           <ThemeProvider>
